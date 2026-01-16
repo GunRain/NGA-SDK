0347712aaa2fe64019297235ed78884fc5b0e681
白彩恋 <shiro@oom-wg.dev>
2026-01-17 02:46:52 +0800

    Update sitemap.tsx

--- docs/src/app/sitemap.tsx
+++ docs/src/app/sitemap.tsx
@@ -5,12 +5,15 @@ import {docsConfig, source} from '@/lib/source'
 export const revalidate = false
 
 export default () =>
-	source.getPages().map(
-		page =>
-			({
-				url: `${docsConfig.baseUrl}${page.url}`,
-				lastModified: page.data.lastModified ?? new Date(),
-				changeFrequency: 'always' as const,
-				priority: page.url === '/' ? 1 : 0.88
-			}) satisfies MetadataRoute.Sitemap[number]
-	) satisfies MetadataRoute.Sitemap
+	source
+		.getPages()
+		.filter(page => !page.data['go-import'] && !page.data['go-source'])
+		.map(
+			page =>
+				({
+					url: `${docsConfig.baseUrl}${page.url}`,
+					lastModified: page.data.lastModified ?? new Date(),
+					changeFrequency: 'always' as const,
+					priority: page.url === '/' ? 1 : 0.88
+				}) satisfies MetadataRoute.Sitemap[number]
+		) satisfies MetadataRoute.Sitemap
