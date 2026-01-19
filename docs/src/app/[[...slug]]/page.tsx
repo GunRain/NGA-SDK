963afc244f740547f1c0ea6476c663ce9925a7a5
白彩恋 <shiro@oom-wg.dev>
2026-01-19 23:38:58 +0800

    Revert "md 2 mdx"
    This reverts commit fcc330aa8580020700039b861b9f4c2064dfdf67.

--- docs/src/app/[[...slug]]/page.tsx
+++ docs/src/app/[[...slug]]/page.tsx
@@ -87,9 +87,9 @@ export default async function Page(props: PageProps<'/[[...slug]]'>) {
 			<DocsDescription className='mb-0'>{page.data.description}</DocsDescription>
 			{!page.data['go-import'] && !page.data['go-source'] && (
 				<div className='flex flex-row gap-2 items-center border-b pb-6'>
-					<LLMCopyButton markdownUrl={`${page.url === '/' ? '/index' : page.url}.mdx`} />
+					<LLMCopyButton markdownUrl={`${page.url === '/' ? '/index' : page.url}.md`} />
 					<ViewOptions
-						markdownUrl={`${page.url === '/' ? '/index' : page.url}.mdx`}
+						markdownUrl={`${page.url === '/' ? '/index' : page.url}.md`}
 						githubUrl={`https://github.com/${docsConfig.git.user}/${docsConfig.git.repo}/blob/${docsConfig.git.branch}/${docsConfig.git.dir ? `${docsConfig.git.dir}/` : ''}content/${page.path}`}
 					/>
 				</div>
