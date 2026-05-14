17d5d163a60f1394a533289599e263b3af3c2ec7
白彩恋 <shiro@oom-wg.dev>
2026-05-15 00:30:43 +0800

    200OK Working

--- docs/src/lib/source.tsx
+++ docs/src/lib/source.tsx
@@ -35,6 +35,10 @@ export const docsConfig = {
 					{
 						label: '世界之外工作组',
 						href: 'https://oom-wg.dev'
+					},
+					{
+						label: '200OK Working',
+						href: 'https://200ok.work'
 					}
 				]
 			}
