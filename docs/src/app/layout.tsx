ab83ac6756e856d935b2703c7790a3f8849789ab
白彩恋 <shiro@oom-wg.dev>
2026-06-07 23:18:36 +0800

    Update
    Signed-off-by: 白彩恋 <shiro@oom-wg.dev>

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -12,7 +12,7 @@ const ubuntu = Ubuntu({ weight: ['300', '400', '500', '700'] })
 
 export default ({ children }: LayoutProps<'/'>) => (
 	<html lang='zh-Hans' className={ubuntu.className} suppressHydrationWarning>
-		<body className='flex flex-col min-h-screen'>
+		<body className='flex min-h-screen flex-col'>
 			<RootProvider
 				search={{ SearchDialog }}
 				i18n={{
